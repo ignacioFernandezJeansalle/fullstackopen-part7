892cf7eb7aa27a6d7e2a20d5b960ea8f22593fe1
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-09 14:28:33 -0300

    Add target blank and rel noreferrer

--- routed-anecdotes/src/components/Footer.jsx
+++ routed-anecdotes/src/components/Footer.jsx
@@ -3,7 +3,11 @@ import "./Footer.css";
 const Footer = () => (
   <footer>
     Anecdote app for <a href="https://fullstackopen.com/">Full Stack Open</a>. See{" "}
-    <a href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js">
+    <a
+      href="https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js"
+      target="_blank"
+      rel="noreferrer"
+    >
       https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js
     </a>{" "}
     for the source code.
