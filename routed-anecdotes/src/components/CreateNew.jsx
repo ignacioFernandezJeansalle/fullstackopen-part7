a1198ea923aa8b5cb204c6e8921c185ef589519b
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-12 09:46:03 -0300

    Use ...rest for optimize props to inputs form (ex7.6)

--- routed-anecdotes/src/components/CreateNew.jsx
+++ routed-anecdotes/src/components/CreateNew.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from "react-router-dom";
 import { useField } from "../hooks";
 
 const CreateNew = ({ addNew }) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -19,9 +19,9 @@ const CreateNew = ({ addNew }) => {
   };
 
   const handleReset = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    resetContent();
+    resetAuthor();
+    resetInfo();
   };
 
   return (
@@ -30,15 +30,15 @@ const CreateNew = ({ addNew }) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input type={content.type} value={content.value} onChange={content.onChange} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input type={author.type} value={author.value} onChange={author.onChange} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input type={info.type} value={info.value} onChange={info.onChange} />
+          <input {...info} />
         </div>
         <button type="submit">create</button>
         <button type="reset" onClick={handleReset}>
