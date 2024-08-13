f240baace2a34d1c13e33a6aa64bca05840e5794
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-13 17:08:33 -0300

    Delete Message component

--- bloglist/client/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import "./Message.css";
-
-export default function Message({ message, isError }) {
-  if (!message) return null;
-
-  const className = isError ? "notification error" : "notification";
-
-  return <div className={className}>{message}</div>;
-}
