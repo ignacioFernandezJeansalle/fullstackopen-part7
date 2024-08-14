ff7fde69213950ea8d4d76fbe0f81c0fc6014ded
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-14 16:42:52 -0300

    Add Link component to /users/id

--- bloglist/client/src/components/Users.jsx
+++ bloglist/client/src/components/Users.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeUsers } from "../reducers/usersReducer";
 
@@ -23,7 +24,9 @@ const Users = () => {
         <tbody>
           {users.map((user) => (
             <tr key={user.id}>
-              <td>{user.name}</td>
+              <td>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </td>
               <td>{user.blogs.length}</td>
             </tr>
           ))}
