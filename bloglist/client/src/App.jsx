44d31a6d1e2cfa970d7daf2c593b13b7007e2050
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-14 15:57:58 -0300

    Add route for /users (ex7.14)

--- bloglist/client/src/App.jsx
+++ bloglist/client/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { initializeBlogs } from "./reducers/blogsReducer";
 import { initializeUser, logoutUser } from "./reducers/userReducer";
+import { Routes, Route, useMatch } from "react-router-dom";
 
 import "./App.css";
 
@@ -9,6 +10,7 @@ import FormLogin from "./components/FormLogin";
 import FormBlog from "./components/FormBlog";
 import Togglable from "./components/Togglable";
 import Blog from "./components/Blog";
+import Users from "./components/Users";
 import Notification from "./components/Notification";
 
 const App = () => {
@@ -29,32 +31,43 @@ const App = () => {
       </header>
 
       <main>
-        <Notification />
         {user === null ? (
           <FormLogin />
         ) : (
-          <>
-            <section className="user-info">
-              <h2>{user.name}</h2>
-              <button onClick={() => dispatch(logoutUser())}>Logout</button>
-            </section>
-
-            <Togglable buttonLabel="Create new blog" ref={formBlogRef}>
-              <FormBlog user={user} hide={() => formBlogRef.current.toggleVisibility()} />
-            </Togglable>
-
-            <section className="list-of-blogs">
-              <h2>Blogs</h2>
-              <ul>
-                {blogs
-                  .toSorted((a, b) => b.likes - a.likes)
-                  .map((blog) => (
-                    <Blog key={blog.id} blog={blog} user={user} />
-                  ))}
-              </ul>
-            </section>
-          </>
+          <section className="user-info">
+            <h2>{user.name}</h2>
+            <button onClick={() => dispatch(logoutUser())}>Logout</button>
+          </section>
         )}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              user && (
+                <>
+                  <Togglable buttonLabel="Create new blog" ref={formBlogRef}>
+                    <FormBlog user={user} hide={() => formBlogRef.current.toggleVisibility()} />
+                  </Togglable>
+
+                  <section className="list-of-blogs">
+                    <h2>Blogs</h2>
+                    <ul>
+                      {blogs
+                        .toSorted((a, b) => b.likes - a.likes)
+                        .map((blog) => (
+                          <Blog key={blog.id} blog={blog} user={user} />
+                        ))}
+                    </ul>
+                  </section>
+                </>
+              )
+            }
+          />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+
+        <Notification />
       </main>
     </>
   );
