df3242c6766434c7b418fc338ba7716845068187
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-14 10:15:43 -0300

    Add modify and remove blogs to reducers

--- bloglist/client/src/reducers/blogsReducer.js
+++ bloglist/client/src/reducers/blogsReducer.js
@@ -7,10 +7,18 @@ const blogSlice = createSlice({
   reducers: {
     setBlogs: (state, action) => action.payload,
     appendBlog: (state, action) => state.concat(action.payload),
+    modifyBlog: (state, action) => {
+      const modifiedBlog = action.payload;
+      return state.map((blog) => (blog.id !== modifiedBlog.id ? blog : modifiedBlog));
+    },
+    removeBlog: (state, action) => {
+      const removedBlog = action.payload;
+      return state.filter((blog) => blog.id !== removedBlog.id);
+    },
   },
 });
 
-export const { setBlogs, appendBlog } = blogSlice.actions;
+export const { setBlogs, appendBlog, modifyBlog, removeBlog } = blogSlice.actions;
 export default blogSlice.reducer;
 
 export const initializeBlogs = () => {
@@ -24,5 +32,19 @@ export const createBlog = (blog, token) => {
   return async (dispatch) => {
     const addedBlog = await blogsServices.create(blog, token);
     dispatch(appendBlog(addedBlog));
+  };
+};
+
+export const updateBlog = (blog, token) => {
+  return async (dispatch) => {
+    const updatedBlog = await blogsServices.update(blog, token);
+    dispatch(modifyBlog(updatedBlog));
+  };
+};
+
+export const deleteBlog = (id, token) => {
+  return async (dispatch) => {
+    const removedBlog = await blogsServices.remove(id, token);
+    dispatch(removeBlog(removedBlog));
   };
 };
