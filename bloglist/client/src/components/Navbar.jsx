0e905f491454390e4c81d237f1b820567c1b510c
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-16 18:11:16 -0300

    Refactor Navbar component with semantic-ui-react (ex7.20)

--- bloglist/client/src/components/Navbar.jsx
+++ bloglist/client/src/components/Navbar.jsx
@@ -1,25 +1,39 @@
-import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { logoutAuthorizedUser } from "../reducers/userAuthorizationReducer";
+import { MenuMenu, MenuItem, Menu, Button, Header } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useAuthorizedUser } from "../hooks";
+import { useState } from "react";
 
-const Navbar = ({ name }) => {
+const Navbar = () => {
+  const [activeItem, setActiveItem] = useState("blogs");
   const dispatch = useDispatch();
+  const { authorizedUser } = useAuthorizedUser();
+
+  if (!authorizedUser) return null;
 
   return (
     <nav>
-      <Link to="/">Blogs</Link>
-      <Link to="/users">Users</Link>
-      <div>
-        <p>{name}</p>
-        <button onClick={() => dispatch(logoutAuthorizedUser())}>Logout</button>
-      </div>
+      <Menu pointing secondary>
+        <Link to="/" className={`item ${activeItem === "blogs" && "active"}`} onClick={() => setActiveItem("blogs")}>
+          Blogs
+        </Link>
+        <Link
+          to="/users"
+          className={`item ${activeItem === "users" && "active"}`}
+          onClick={() => setActiveItem("users")}
+        >
+          Users
+        </Link>
+        <MenuMenu position="right">
+          <p className="item">{authorizedUser.name}</p>
+          <MenuItem>
+            <Button onClick={() => dispatch(logoutAuthorizedUser())}>Logout</Button>
+          </MenuItem>
+        </MenuMenu>
+      </Menu>
     </nav>
   );
-};
-
-Navbar.propTypes = {
-  name: PropTypes.string.isRequired,
 };
 
 export default Navbar;
