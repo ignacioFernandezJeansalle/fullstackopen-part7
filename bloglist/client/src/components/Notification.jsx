d800c72e82348383b31954134cb1efb0d8e81628
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-17 14:36:28 -0300

    Refactor Norification component with semantic-ui-react (ex7.20)

--- bloglist/client/src/components/Notification.jsx
+++ bloglist/client/src/components/Notification.jsx
@@ -1,11 +1,16 @@
 import { useSelector } from "react-redux";
+import { Message } from "semantic-ui-react";
 
 const Notification = () => {
   const [content, error] = useSelector(({ notification }) => notification);
 
-  if (content === "") return null;
+  const show = content !== "";
 
-  return <div>{content}</div>;
+  return (
+    <Message hidden={!show} visible={show} negative={error} positive={!error}>
+      {content}
+    </Message>
+  );
 };
 
 export default Notification;
