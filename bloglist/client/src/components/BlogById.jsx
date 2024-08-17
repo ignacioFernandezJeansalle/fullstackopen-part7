7cb0f618c4dec055f4ba423151d0fab9724b828c
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-17 14:37:36 -0300

    Refactor BlogById component with semantic-ui-react (ex7.20)

--- bloglist/client/src/components/BlogById.jsx
+++ bloglist/client/src/components/BlogById.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { updateBlog, deleteBlog, addComment } from "../reducers/blogsReducer";
 import { useAuthorizedUser } from "../hooks";
+import { Header, Label, Icon, Button, Form, FormField, Input, List, ListItem } from "semantic-ui-react";
 
 const BlogById = ({ blog }) => {
   const dispatch = useDispatch();
   const { authorizedUser } = useAuthorizedUser();
   const [comment, setComment] = useState("");
+  const navigate = useNavigate();
 
   if (!blog || !authorizedUser) return null;
 
@@ -14,7 +17,10 @@ const BlogById = ({ blog }) => {
 
   const addLike = () => dispatch(updateBlog({ ...blog, likes: blog.likes + 1 }, authorizedUser.token));
 
-  const remove = () => dispatch(deleteBlog(blog, authorizedUser.token));
+  const remove = () => {
+    dispatch(deleteBlog(blog, authorizedUser.token));
+    navigate("/");
+  };
 
   const submitComment = (event) => {
     event.preventDefault();
@@ -24,39 +30,53 @@ const BlogById = ({ blog }) => {
 
   return (
     <section>
-      <h2>
+      <Header as="h2">
         {title} by {author}
-      </h2>
+      </Header>
       <a href={url} target="_blank" rel="noreferrer">
         {url}
       </a>
-      <p>{likes} likes</p>
-      <button onClick={addLike}>Like</button>
+      <div>
+        <Label>
+          {likes} <Icon name="heart" />
+        </Label>
+        <Button onClick={addLike}>Like</Button>
+      </div>
       <p>Added by {user.name}</p>
       {blog.user.id === authorizedUser.id && (
         <button
           onClick={remove}
+          className="ui button"
           style={{
-            marginTop: "12px",
-            padding: "2px 4px",
             background: "#c00",
-            border: "1px solid #800",
             color: "#fff",
           }}
         >
+          <Icon name="trash" />
           Remove
         </button>
       )}
-      <h3>Comments</h3>
-      <form onSubmit={submitComment}>
-        <input type="text" value={comment} onChange={({ target }) => setComment(target.value)} />
-        <button type="submit">Add comment</button>
-      </form>
-      <ul>
+
+      <Header as="h3">Comments</Header>
+
+      <Form onSubmit={submitComment}>
+        <FormField>
+          <Input
+            size="small"
+            label="Comment"
+            type="text"
+            value={comment}
+            onChange={({ target }) => setComment(target.value)}
+          />
+        </FormField>
+        <Button type="submit">Add comment</Button>
+      </Form>
+
+      <List celled>
         {comments.map((comment) => (
-          <li key={comment.id}>{comment.content}</li>
+          <ListItem key={comment.id}>{comment.content}</ListItem>
         ))}
-      </ul>
+      </List>
     </section>
   );
 };
