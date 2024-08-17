f82e57450c3b16bf2417fa6be933eaf2d1861a6f
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-08-17 14:37:20 -0300

    Refactor BlogList component with semantic-ui-react (ex7.20)

--- bloglist/client/src/components/BlogList.jsx
+++ bloglist/client/src/components/BlogList.jsx
@@ -1,17 +1,25 @@
 import PropTypes from "prop-types";
-import BlogListItem from "./BlogListItem";
+import { Header, List, ListItem, ListIcon, ListContent } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs }) => {
   return (
     <section>
-      <h2>Blogs</h2>
-      <ul>
+      <Header as="h2">Blogs</Header>
+      <List>
         {blogs
           .toSorted((a, b) => b.likes - a.likes)
           .map((blog) => (
-            <BlogListItem key={blog.id} blog={blog} />
+            <ListItem key={blog.id}>
+              <ListIcon name="check square outline" />
+              <ListContent>
+                <Link to={`/blogs/${blog.id}`}>
+                  <b>{blog.title.toUpperCase()}</b> - {blog.author}
+                </Link>
+              </ListContent>
+            </ListItem>
           ))}
-      </ul>
+      </List>
     </section>
   );
 };
